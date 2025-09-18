b9e487a41d4a298684bbc3facf545dbc1062b57a
Michał Komsa <komsa.m@o2.pl>
2025-09-18 19:22:36 +0200

    particles and sounds added

--- resources/js/Pages/Homework/Show.vue
+++ resources/js/Pages/Homework/Show.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {Link, router, useForm} from "@inertiajs/vue3";
 import AppLayout from "@/Layouts/AppLayout.vue";
-import {ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import JetButton from "@/Components/Button.vue";
 import JetLabel from "@/Components/Label.vue";
 import JetInput from "@/Components/Input.vue";
@@ -10,7 +10,8 @@ import ReturnButton from "@/Components/ReturnButton.vue";
 import Toast from "@/Components/Toast.vue";
 import {useToast} from "vue-toastification";
 import TextArea from "@/Components/TextArea.vue";
-import DialogModal from "@/Components/DialogModal.vue";
+import ConfettiExplosion from "vue-confetti-explosion"
+import Fireworks from "@fireworks-js/vue";
 
 const props = defineProps({
     item: Object,
@@ -18,13 +19,33 @@ const props = defineProps({
 });
 
 const toast = useToast();
+const explodingTriggers = ref([]);
+let correctExerciseAudio, finishHomeworkAudio;
+const fw = ref();
+const fireworksActive = ref(false);
+const options = ref({
+    opacity: 0.5,
+    sound: {
+        explosion: 100,
+        enabled: true,
+        files: [
+            '/explosion0.mp3',
+            '/explosion1.mp3',
+            '/explosion2.mp3',
+        ],
+        volume: {
+            min: 100,
+            max: 100,
+        }
+    },
+})
 
 let form = useForm({
     id: props.item.id,
     title: props.item.title,
 });
 
-const checkExercise = (id) => {
+const checkExercise = (id, order) => {
     const answer = document.querySelector('#answer-' + id);
 
     router.patch(route('exercises.check', id), {
@@ -38,6 +59,19 @@ const checkExercise = (id) => {
                     operation: 'Operacja zakończona powodzeniem!'
                 }
             });
+            
+            if (props.item.complete_date) {
+                finishHomeworkAudio.play();
+                fireworksActive.value = true;
+                fw.value.start();
+                setTimeout(() => {
+                    fw.value.stop();
+                    fireworksActive.value = false;
+                }, 15000);
+            } else {
+                explodingTriggers.value[order] = true;
+                correctExerciseAudio.play();
+            }
         },
         onError: () => {
             toast.error({
@@ -51,7 +85,17 @@ const checkExercise = (id) => {
     })
 }
 
+onMounted(() => {
+    props.item.exercises.forEach(() => {
+        explodingTriggers.value.push(false);
+    });
 
+    correctExerciseAudio = new Audio("/success.mp3");
+    correctExerciseAudio.preload = "auto";
+
+    finishHomeworkAudio = new Audio('/applauz.mp3');
+    finishHomeworkAudio.preload = 'auto';
+})
 </script>
 
 <template>
@@ -68,31 +112,41 @@ const checkExercise = (id) => {
             </h2>
         </template>
 
-        <form @submit.prevent="submitForm()">
-            <div class="pt-12 pb-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div
-                    class="bg-white dark:bg-gray-800 dark:text-gray-400 text-gray-800 overflow-hidden shadow-xl sm:rounded-lg py-4 px-4">
-                    <div class="flex flex-col">
-                        <div class="p-6">
+        <Fireworks
+            ref="fw"
+            :autostart="false"
+            v-show="fireworksActive"
+            :options="options"
+            :style="{
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              position: 'fixed'
+            }"
+        />
 
-                            <div>
-                                <JetLabel for="title" value="Tytuł"/>
-                                <JetInput id="title" v-model="form.title" type="text" placeholder="Tytuł"
-                                          class="block w-full" disabled/>
-                                <div class="error-message" v-if="errors.title">{{ errors.title }}</div>
-                            </div>
+        <div class="pt-12 pb-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div
+                class="bg-white dark:bg-gray-800 dark:text-gray-400 text-gray-800 overflow-hidden shadow-xl sm:rounded-lg py-4 px-4">
+                <div class="flex flex-col">
+                    <div class="p-6">
 
-                            <div class="flex justify-end mt-5">
-
-                                <ReturnButton/>
-
-                            </div>
-
+                        <div>
+                            <JetLabel for="title" value="Tytuł"/>
+                            <JetInput id="title" v-model="form.title" type="text" placeholder="Tytuł"
+                                      class="block w-full" disabled/>
+                            <div class="error-message" v-if="errors.title">{{ errors.title }}</div>
                         </div>
+
+                        <div class="flex justify-end mt-5">
+                            <ReturnButton/>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
 
         <div class="pb-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div
@@ -141,13 +195,19 @@ const checkExercise = (id) => {
                             <TextArea style="width: 100%" class="mt-2" v-model="exercise.assignment"
                                       disabled></TextArea>
 
+                            <div class="text-center flex justify-center">
+                                <ConfettiExplosion v-if="explodingTriggers[index]"
+                                                   @complete="explodingTriggers[index] = false"/>
+                            </div>
+
+
                             <template v-if="exercise.complete_date">
                                 <JetInput :id="'answer-' + exercise.id" :value="exercise.answer" type="text"
                                           placeholder="Odpowiedź"
                                           class="block w-full" disabled/>
                             </template>
                             <template v-else>
-                                <form @submit.prevent="() => checkExercise(exercise.id)">
+                                <form @submit.prevent="() => checkExercise(exercise.id, index)">
                                     <div class="flex justify-between items-center">
                                         <JetInput :id="'answer-' + exercise.id" type="text"
                                                   placeholder="Odpowiedź"
